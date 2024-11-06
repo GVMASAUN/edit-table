a606fc3d2f4086cf3b67d8771cc2672ddb3ec106
Gautam Verma <gautam.verma@thecodeinsight.com>
2024-11-06 13:03:16 +0530

    Moved to Exercise-02/Backend

--- Gautam Verma/canteen-management-system/src/main/java/com/codeinsight/exercise/repository/OrderDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeinsight.exercise.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codeinsight.exercise.entity.OrderDetails;
-
-public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long>{
-
-}
