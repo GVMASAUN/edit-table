4000fb50a9ee9d56c40232e62e59ecb92a2d584a
Gautam Verma <vermagautam4956@gmail.com>
2024-11-19 10:28:19 +0530

    displaying backend errors in view mode only(not in edit mode)

--- Gautam Verma/angular/dependency/src/app/table-edit/table-edit.component.ts
+++ Gautam Verma/angular/dependency/src/app/table-edit/table-edit.component.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DataService } from '../data.service';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-table-edit',
   templateUrl: './table-edit.component.html',
   styleUrls: ['./table-edit.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableEditComponent {
   tableColumns: any;
   tableItems: any;
   mainForm!: FormGroup;
-  formGroupArray!: FormArray;
-  selected: FormGroup = this.formBuilder.group({});
-
+  formGroupArray!: FormArray<FormGroup>;
+  selected: FormGroup | null = null;
+  dataRows: any;
+  initFlag: boolean = false;
+  responseData: any;
 
   constructor(private dataService: DataService, private formBuilder: FormBuilder) {
     this.tableColumns = this.dataService.getTableColumns();
     this.tableItems = this.dataService.getTableItems();
+    this.responseData = dataService.responseData;
 
     this.mainForm = this.formBuilder.group({
       formGroupArray: this.formBuilder.array([])
@@ -26,55 +30,101 @@ export class TableEditComponent {
 
   ngOnInit(): void {
     this.formGroupArray = this.mainForm.get('formGroupArray') as FormArray;
+    let group: FormGroup;
 
     this.tableItems?.forEach((item: any) => {
-      this.formGroupArray.push(
-        this.formBuilder.group({
-          propcol1: [item.propcol1, [Validators.required, Validators.minLength(3), Validators.pattern("^[a-zA-Z ]+$")]],
-          propcol2: [item.propcol2, [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-          propcol3: [item.propcol3, [Validators.required, Validators.email]],
-          propcol4: [item.propcol4, [Validators.required, Validators.email]],
-          propcol5: [item.propcol5, [Validators.required, Validators.email]],
-          propcol6: [item.propcol6, [Validators.required, Validators.email]]
-        })
-      );
+
+      //alternate:
+      // this.feedbackForm = new FormArray(this.data.map(x => new FormGroup({
+      //   'question': new FormControl(x.description),
+      //   'answer': new FormControl('')
+      // })))
+      //or
+      // this.tableForm = this.fb.group({
+      //   arrayForm: this.fb.array(this.results.map(r => this.fb.group(r)))
+      // });
+
+      group = this.formBuilder.group({
+        id: [item.id, [Validators.required]],
+        first_name: [item.first_name, [Validators.required, Validators.pattern("^[a-zA-Z ]+$")]],
+        last_name: [item.last_name, [Validators.required, Validators.pattern("^[a-zA-Z ]+$")]],
+        email: [item.email, [Validators.required, Validators.email]],
+        gender: [item.gender, [Validators.required]],
+        ip_address: [item.ip_address, [Validators.required]]
+      });
+
+      this.formGroupArray.push(group);
     });
-    this.tableItems?.forEach((item: any) => {
-      this.formGroupArray.push(
-        this.formBuilder.group({
-          propcol1: [item.propcol1, [Validators.required, Validators.minLength(3), Validators.pattern("^[a-zA-Z ]+$")]],
-          propcol2: [item.propcol2, [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-          propcol3: [item.propcol3, [Validators.required, Validators.email]],
-          propcol4: [item.propcol4, [Validators.required, Validators.email]],
-          propcol5: [item.propcol5, [Validators.required, Validators.email]],
-          propcol6: [item.propcol6, [Validators.required, Validators.email]]
-        })
-      );
-    }); this.tableItems?.forEach((item: any) => {
-      this.formGroupArray.push(
-        this.formBuilder.group({
-          propcol1: [item.propcol1, [Validators.required, Validators.minLength(3), Validators.pattern("^[a-zA-Z ]+$")]],
-          propcol2: [item.propcol2, [Validators.required, Validators.pattern("^[0-9]{10}$")]],
-          propcol3: [item.propcol3, [Validators.required, Validators.email]],
-          propcol4: [item.propcol4, [Validators.required, Validators.email]],
-          propcol5: [item.propcol5, [Validators.required, Validators.email]],
-          propcol6: [item.propcol6, [Validators.required, Validators.email]]
-        })
-      );
-    });
+
+    this.dataRows = this.formGroupArray.controls as FormGroup[];
+    this.initFlag = true;
   }
 
-  edit(rowData: FormGroup): void {
-    if (this.selected.valid) {
+  edit(rowData: any): void {
+    if (!this.selected) {
       this.selected = rowData;
+    }
+    debugger;
+    // this.patchErrors(rowData);
+  }
+
+  // private patchErrors(group: FormGroup): void {
+  //   const item = this.responseData.find((i: { id: number, errors?: { [key: string]: string } }) => i.id === group.value.id);
+  //   if (item.errors) {
+  //     for (let field in item.errors) {
+  //       if (item.errors.hasOwnProperty(field)) {
+  //         const control = group.get(field);
+  //         if (control) {
+  //           control.setErrors({
+  //             backendError: item.errors[field]
+  //           });
+  //           control.markAsDirty();
+  //           control.markAsTouched();
+  //           // control.disable();
+  //           // control.updateValueAndValidity();
+  //         }
+
+  //       }
+  //     }
+  //   }
+  // }
+
+  private patchResponse(responseData: any): void {
+    for (let iterator = 0; iterator < responseData.length; ++iterator) {
+      let group: FormGroup = this.formGroupArray.at(iterator);
+      const item = responseData[iterator];
+      group.patchValue(item);
+
+      if (item.errors) {
+        for (let field in item.errors) {
+          if (item.errors.hasOwnProperty(field)) {
+            const control = group.get(field);
+            if (control) {
+              control.setErrors({
+                backendError: item.errors[field]
+              });
+              control.markAsDirty();
+              // control.markAsTouched();
+              // control.disable();
+              // control.updateValueAndValidity();
+            }
+
+          }
+        }
+      }
     }
   }
 
+  trackById(index: number, formGroup: any): number {
+    return index;
+  }
+
   save(): void {
-    this.selected = this.formBuilder.group({});
+    this.selected = null;
   }
 
   submit(): void {
     console.log(this.mainForm.value);
+    this.patchResponse(this.dataService.responseData);
   }
 }
